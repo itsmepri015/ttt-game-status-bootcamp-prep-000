218180a36d42534a67ddfa84cdb7fb7bf2c37071
naizal Edy <pritiadhikari@gmail.com>
2018-10-15 19:16:38 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,8 +21,7 @@ def won(board)
   
   
   for each win_combination in WIN_COMBINATIONS
-  # win_combination is a 3 element array of indexes that compose a win, [0,1,2]
-  # grab each index from the win_combination that composes a win.
+  
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
